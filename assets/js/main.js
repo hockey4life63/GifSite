a62c025524bf11d9a01148a385704e560860e071
hockey4life63 <Amelancon68@gmail.com>
2017-05-14 13:37:11 -0700

    now with more style

--- assets/js/main.js
+++ assets/js/main.js
@@ -2,7 +2,9 @@ let gifObj = {
     apiKey: "api_key=dc6zaTOxFJmzC",
     url: "https://api.giphy.com/v1/gifs/search?",
     limit: "limit=10&",
-    topics: ["cats", "dogs", "birds"],
+    topics: ["Breaking Bad", "Curb Your Enthusiasm", "West Wing", "Longmire", "The x-Files",
+        "SNL"
+    ],
     apiCall: function(query) {
         let search = "q=" + query + "&";
         let queryURL = this.url + this.limit + search + this.apiKey;
@@ -11,8 +13,8 @@ let gifObj = {
             method: "get"
         }).done(function(response) {
             $(".gifContainer").empty();
+            $(".gifContainer").append($("<h1>").text(query).addClass("gifTitle"));
             for (var i = 0; i < response.data.length; i++) {
-                console.log(response.data[i])
                 gifObj.buildGifs(response.data[i]);
             }
             gifObj.addGifLisnter();
@@ -67,10 +69,22 @@ let gifObj = {
                 img.attr("src", img.attr("data-still"));
             }
         })
+    },
+    startUpCall: function() {
+        $.ajax({
+            url: "http://api.giphy.com/v1/gifs/trending?limit=10&api_key=dc6zaTOxFJmzC",
+            method: "get"
+        }).done(function(response) {
+            for (var i = 0; i < response.data.length; i++) {
+                gifObj.buildGifs(response.data[i]);
+            }
+            gifObj.addGifLisnter();
+        })
     }
 
 }
 gifObj.buildButtons();
+gifObj.startUpCall();
 
 $("#addBtn").on("click", function(event) {
     event.preventDefault();
@@ -78,4 +92,5 @@ $("#addBtn").on("click", function(event) {
     if (name !== "") {
         gifObj.addButton(name);
     }
+    $("#addBtnName").val("")
 })
